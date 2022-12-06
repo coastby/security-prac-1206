f2dd45b7755fb88e6ad9534c9bb8f7fcbf8ae1cd
JO YEJI <whdpwl2@gmail.com>
2022-12-06 13:26:57 +0900

    Feat: SecurityFilterChain 설정

---
+++ src/main/java/com/example/prac1206/configuration/SecurityConfig.java
@@ -0,0 +1,35 @@
+package com.example.prac1206.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable()
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/users", "/api/v1/users/login").permitAll()    //가입, 로그인은 허용
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()         //나머지는 막음
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+//                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
+}
